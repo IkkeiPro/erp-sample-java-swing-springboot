e89c962b1514716240adc1b5ed77f5ea8802d9f7
IkkeiPro <39822487+IkkeiPro@users.noreply.github.com>
2026-03-05 22:22:42 +0900

    Refactor CompositionRow bean and move mapper SQL to XML

--- src/main/java/com/ikkei/swingapp/domain/CompositionRow.java
+++ src/main/java/com/ikkei/swingapp/domain/CompositionRow.java
@@ -1,4 +1,32 @@
 package com.ikkei.swingapp.domain;
 
-public record CompositionRow(String parentPartNo, String childPartNo, Integer level) {
+public class CompositionRow {
+
+    private String parentPartNo;
+    private String childPartNo;
+    private Integer level;
+
+    public String getParentPartNo() {
+        return parentPartNo;
+    }
+
+    public void setParentPartNo(String parentPartNo) {
+        this.parentPartNo = parentPartNo;
+    }
+
+    public String getChildPartNo() {
+        return childPartNo;
+    }
+
+    public void setChildPartNo(String childPartNo) {
+        this.childPartNo = childPartNo;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
 }
